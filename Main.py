26059127e586b021ca651886eb5761ef5112f585
Joe <JOE.TATE24@bathspa.ac.uk>
2025-09-24 11:23:08 +0100

    Class Challenge 1 data structures

---
+++ Main.py
@@ -0,0 +1,23 @@
+my_list = [10, "twenty", 30, "forty", 50]
+print(my_list[0, 3])
+      
+my_list.append(60)
+print(my_list)
+
+grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(grid[1][2])
+
+my_tuple = (1, 2, 3, 4)
+print(my_tuple[1])
+
+my_dict = {
+    "name": "Alex",
+    "age": 17,
+    "subjects": ["English", "Math", "Computing"]
+}
+print(my_dict["name"])
+
